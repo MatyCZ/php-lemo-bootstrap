ce66ab1f6c94838579390e2a10de02b1b3765e62
Matej Szendi <matej.szendi@bcom.cz>
2013-04-30 16:06:31 +0200

    * Repair label position

--- src/LemoBootstrap/Form/View/Helper/FormLabel.php
+++ src/LemoBootstrap/Form/View/Helper/FormLabel.php
@@ -119,7 +119,7 @@ class FormLabel extends AbstractHelper
             if (empty($label)) {
                 throw new Exception\DomainException(sprintf(
                     '%s expects either label content as the second argument, ' .
-                    'or that the element provided has a label attribute; neither found',
+                        'or that the element provided has a label attribute; neither found',
                     __METHOD__
                 ));
             }
